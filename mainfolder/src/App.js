20344583f640494837c7bb495c02236f7c0c0262
arpit2444 <arpitjain2444@gmail.com>
2022-12-15 16:07:27 +0530

    day-2

---
+++ mainfolder/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar1 from './Components/Navbar1';
+import Navbar2 from './Components/Navbar2';
+
+function App() {
+  return (
+    <div className="App">
+  <Navbar2/>
+    </div>
+  );
+}
+
+export default App;
---
+++ mainfolder/src/Components/Navbar1.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navbar1() {
+  return (
+    <div>
+        <div>
+            <span>UNITED STATE</span>
+            <span>CUSTOMER CARE</span>
+        </div>
+        <div>
+            <span></span>
+            <span>CUSTOMER CARE</span>
+        </div>
+    </div>
+  )
+}
