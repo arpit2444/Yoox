8f549c80399d83a5111f22d7d9b9166da80ad1c7
arpit2444 <arpitjain2444@gmail.com>
2022-12-19 15:59:36 +0530

    EQ

--- mainfolder/src/Pages/AllRoute.jsx
+++ mainfolder/src/Pages/AllRoute.jsx
@@ -6,6 +6,7 @@ import Home from './Home'
 import Login from './Login'
 import Product from './Product'
 import Register from './Register'
+import NotFound from './NotFound'
 export default function AllRoute() {
   return (
         <Routes>
@@ -14,7 +15,7 @@ export default function AllRoute() {
             <Route path='/register' element={<Register/>} ></Route>
             <Route path='/cart' element={<Cart/>} ></Route>
             <Route path='/product' element={<Product/>} ></Route>
-            <Route path="*" element={<Notfound />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
         </Routes>
 
   )
